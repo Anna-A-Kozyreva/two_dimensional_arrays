95c185c6a71adc051c5130efb0902054e4e7ee56
Anna <annakoz1519@yandex.ru>
2023-12-18 02:09:10 +0300

    added task arrays and strings

--- Program.cs
+++ Program.cs
@@ -5,51 +5,78 @@
 // что такого элемента нет.
 
 
-int[,] CreateMatrix(int rows, int columns, int min, int max)
+// int[,] CreateMatrix(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rand = new Random();
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             matrix[i, j] = rand.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// int[,] res = CreateMatrix(4, 4, 0, 10);
+
+// PrintMatrix(res);
+
+// int row = res.GetLength(0);
+// int col = res.GetLength(1);
+
+
+// int GetElementValue(int[,] res, int row, int col)
+// {
+//     if (row < res.GetLength(0) && col < res.GetLength(1))
+//     {
+//         return res[row, col];
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+// Console.WriteLine(GetElementValue(res,1,0));
+
+//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+
+string result = CreateStringFromArray(charArray);
+Console.WriteLine(result);
+
+string CreateStringFromArray(char[,] charArray)
 {
-    int[,] matrix = new int[rows, columns];
-    Random rand = new Random();
-    for (int i = 0; i < rows; i++)
+    string result = "";
+
+    for (int i = 0; i < charArray.GetLength(0); i++)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < charArray.GetLength(1); j++)
         {
-            matrix[i, j] = rand.Next(min, max + 1);
+            result += charArray[i, j];
         }
     }
-    return matrix;
+    return result;
 }
 
-void PrintMatrix(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($"{matr[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
-}
+//Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
 
-
-int[,] res = CreateMatrix(4, 4, 0, 10);
-
-PrintMatrix(res);
-
-int row = res.GetLength(0);
-int col = res.GetLength(1);
-
-
-int GetElementValue(int[,] res, int row, int col)
-{
-    if (row < res.GetLength(0) && col < res.GetLength(1))
-    {
-        return res[row, col];
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-Console.WriteLine(GetElementValue(res,1,0));
+string input = "ZDfR1PfE-";
+string result = input.ToLower();
+Console.WriteLine(result);
